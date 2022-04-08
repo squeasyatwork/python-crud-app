81a6a1dd1995f94b4e8ed9ad4025247ce91526ad
amal0055 <amal0055@student.monash.edu>
2022-04-08 15:48:07 +1000

    START: Downloaded and unzipped the Ass2 template

---
+++ Assessments/Assignment02/Main.py
@@ -0,0 +1,27 @@
+
+
+def show_menu():
+    pass
+
+
+def process_operations():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    # print a welcome message
+
+    # manually register admin
+
+    main()
+
+
+
+
+
+
+
